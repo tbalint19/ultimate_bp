a9712da6b82bc6699a7a1b931823d1f088eaa98d
tbalint19 <toth910719balint@gmail.com>
2017-07-28 21:19:17 +0200

    app hello world

---
+++ admin/frontend-dev-env/webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+
+  entry: {
+     app: './main.jsx',
+  },
+
+  output: {
+      path: '../../admin/Static/static/Apps/',
+      filename: "[name].js",
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ],
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'assets/css', 'assets/js'],
+    extensions: ['', '.js', '.jsx', '.css']
+  },
+}
---
+++ app/Website-App/Static/__init__.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class StaticConfig(AppConfig):
+    name = 'Static'
