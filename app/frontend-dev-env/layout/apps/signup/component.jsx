b6f3ed10b27a0ca2af64ac35b3445dc6254c2a6b
tbalint19 <toth910719balint@gmail.com>
2017-07-30 20:12:50 +0200

    common style

--- app/frontend-dev-env/layout/apps/signup/component.jsx
+++ app/frontend-dev-env/layout/apps/signup/component.jsx
@@ -9,28 +9,38 @@ class Signup extends AppComponent {
     let passwordAgain = this._state.signup.passwordAgain
     return (
       <div className={"signup-component-container"}>
+        <div className={"intro-container"}></div>
         <div className={"signup-container"}>
-          <input
-            onChange={(event)=>this.changeInputField("signup.username", event)}
-            placeholder={"Username"}/>
-          <input
-            onChange={(event)=>this.changeInputField("signup.email", event)}
-            placeholder={"Email"}/>
-          <input
-            onChange={(event)=>this.changeInputField("signup.password", event)}
-            placeholder={"Password"}/>
-          <input
-            onChange={(event)=>this.changeInputField("signup.passwordAgain", event)}
-            placeholder={"Password again"}/>
-          <button
-            disabled={
-              username.length < 6 ||
-              email.lenght< 6 || !email.includes("@") || !email.includes(".") ||
-              password.length < 6 || password != passwordAgain
-            }
-            onClick={()=>this.signup(username, email, password)}>
-            Signup
-          </button>
+          <div className={"card"}>
+            <input
+              className={"default-input"}
+              onChange={(event)=>this.changeInputField("signup.username", event)}
+              placeholder={"Username"}/>
+            <input
+              className={"default-input"}
+              onChange={(event)=>this.changeInputField("signup.email", event)}
+              placeholder={"Email"}/>
+            <input
+              className={"default-input"}
+              type={"password"}
+              onChange={(event)=>this.changeInputField("signup.password", event)}
+              placeholder={"Password"}/>
+            <input
+              className={"default-input"}
+              type={"password"}
+              onChange={(event)=>this.changeInputField("signup.passwordAgain", event)}
+              placeholder={"Password again"}/>
+            <button
+              className={"default-button"}
+              disabled={
+                username.length < 6 ||
+                email.lenght< 6 || !email.includes("@") || !email.includes(".") ||
+                password.length < 6 || password != passwordAgain
+              }
+              onClick={()=>this.signup(username, email, password)}>
+              Signup
+            </button>
+          </div>
         </div>
       </div>
     )
